f0ebf1cf28a72bad688d12d3f776114e957868cd
Umang-ussco <me.umang.ca@gmail.com>
2025-03-11 19:03:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import InvoiceGenerator from "./InvoiceGenerator";
+
+function App() {
+  return (
+    <div>
+      <h1>Invoice Management System</h1>
+      <InvoiceGenerator />
+    </div>
+  );
+}
+
+export default App;
